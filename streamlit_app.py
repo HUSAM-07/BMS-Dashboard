2986562016126364775d1f32e1e657fe4a414e20
Mohammed Husamuddin <uni.husam@gmail.com>
2023-06-02 11:07:15 +0400

    Wide Layout_Page Config

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
 # Create a list to store tasks
 tasks = []
 
