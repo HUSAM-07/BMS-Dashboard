5a0033e2782e57a923bc497b5e83c36026779e31
Mohammed Husamuddin <uni.husam@gmail.com>
2023-06-02 11:34:10 +0400

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,37 @@
 import streamlit as st
 import pandas as pd
 
-# Create a dataframe to store tasks
-tasks_df = pd.DataFrame(columns=['Task Name', 'Department', 'Assigned To', 'Deadline', 'Details', 'Status'])
-
 # Function to add a new task
-def add_task(task_name, department, assigned_to, deadline, details):
-    global tasks_df
-    new_task = pd.Series([task_name, department, assigned_to, deadline, details, 'Pending'], index=tasks_df.columns)
+def add_task(task_name, department, assigned_to, deadline, details, tasks_df):
+    new_task = pd.Series(
+        [task_name, department, assigned_to, deadline, details, 'Pending'],
+        index=tasks_df.columns
+    )
     tasks_df = tasks_df.append(new_task, ignore_index=True)
+    return tasks_df
 
 # Function to update task status
-def update_task_status(index, status):
-    global tasks_df
+def update_task_status(index, status, tasks_df):
     tasks_df.at[index, 'Status'] = status
+    return tasks_df
 
 # Function to delete a task
-def delete_task(index):
-    global tasks_df
+def delete_task(index, tasks_df):
     tasks_df = tasks_df.drop(index)
+    tasks_df.reset_index(drop=True, inplace=True)
+    return tasks_df
 
 # Function to display tasks
-def display_tasks():
+def display_tasks(tasks_df):
     st.subheader('Tasks List')
     st.dataframe(tasks_df)
 
 # Main function
 def main():
     st.title('Tasks Tracker')
+
+    # Load existing tasks from a file or database
+    tasks_df = pd.DataFrame(columns=['Task Name', 'Department', 'Assigned To', 'Deadline', 'Details', 'Status'])
 
     task_name = st.text_input('Task Name')
     department = st.selectbox('Assigned to Department', ['Department A', 'Department B', 'Department C'])
@@ -36,19 +40,19 @@ def main():
     details = st.text_area('Details')
 
     if st.button('Add Task'):
-        add_task(task_name, department, assigned_to, deadline, details)
+        tasks_df = add_task(task_name, department, assigned_to, deadline, details, tasks_df)
         st.success('Task added successfully!')
 
-    display_tasks()
+    display_tasks(tasks_df)
 
     if st.checkbox('Mark Task as Completed'):
         completed_task_index = st.number_input('Enter the index of the task to mark as completed', min_value=0, max_value=len(tasks_df)-1, value=0, step=1)
-        update_task_status(completed_task_index, 'Completed')
+        tasks_df = update_task_status(completed_task_index, 'Completed', tasks_df)
         st.success('Task status updated!')
 
     if st.checkbox('Delete Task'):
         delete_task_index = st.number_input('Enter the index of the task to delete', min_value=0, max_value=len(tasks_df)-1, value=0, step=1)
-        delete_task(delete_task_index)
+        tasks_df = delete_task(delete_task_index, tasks_df)
         st.success('Task deleted successfully!')
 
 if __name__ == '__main__':
