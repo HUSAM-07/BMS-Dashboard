86bff7786d37655025ed07b5b0403cff95903d63
Mohammed Husamuddin <uni.husam@gmail.com>
2023-06-02 10:15:30 +0400

    Fields Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,12 @@ import streamlit as st
 # Create a list to store tasks
 tasks = []
 
+# Departments
+departments = ['Department A', 'Department B', 'Department C']
+
+# Assignees
+assignees = ['John', 'Jane', 'Mike', 'Emily']
+
 # Function to add a task
 def add_task(task):
     tasks.append(task)
@@ -19,10 +25,19 @@ def main():
     # Sidebar to add new tasks
     st.sidebar.header("Add New Task")
     new_task = st.sidebar.text_input("Enter task")
+    department = st.sidebar.selectbox("Department Assigned To", departments)
+    assigned_to = st.sidebar.multiselect("Assigned To", assignees)
+    date_to_be_completed = st.sidebar.date_input("Date to be Completed")
+    task_description = st.sidebar.text_area("Task Description")
     add_button = st.sidebar.button("Add Task")
 
     if add_button and new_task != "":
-        add_task(new_task)
+        task = f"**Task:** {new_task}\n" \
+               f"**Department:** {department}\n" \
+               f"**Assigned To:** {', '.join(assigned_to)}\n" \
+               f"**Date to be Completed:** {date_to_be_completed}\n" \
+               f"**Task Description:** {task_description}"
+        add_task(task)
         st.sidebar.success("Task added successfully!")
 
     # Display the list of tasks
