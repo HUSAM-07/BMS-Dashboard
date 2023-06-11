6cd6feff6abdcd55f1c3bd488c31864a55e490c8
Mohammed Husamuddin <uni.husam@gmail.com>
2023-06-11 20:40:07 +0400

    Updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,22 @@
 import streamlit as st
+import pickle
+from pathlib import Path
 import streamlit_authenticator as stauth
-import yaml
-from yaml.loader import SafeLoader
 
-# Load the YAML file
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
+# Load the hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
 
-# Create the authenticator object
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
+authenticator = stauth.Authenticate(cookie_expiry_days=1)
 
-# Render the login widget
-name, authentication_status, username = authenticator.login('Login', 'main')
+name, authentication_status, username = authenticator.login("Login", "sidebar")
 
-if authentication_status:
-    authenticator.logout('Logout', 'main')
-    st.write(f'Welcome {name}')
-    st.title('Organization Task Dashboard')
-
+if authentication_status is False:
+    st.error("The Username/Password Entered is Incorrect, Please Contact HUSAM for Your LogIn Credentials")
+elif authentication_status is None:
+    st.warning("Please enter your credentials to login")
+elif authentication_status:
     # Create a list to store tasks
     tasks = []
 
@@ -44,22 +37,33 @@ if authentication_status:
 
     # Streamlit app layout
     def main():
-        st.header("Add New Task")
-        new_task = st.text_input("Enter task")
-        department = st.selectbox("Department Assigned To", departments)
-        assigned_to = st.multiselect("Assigned To", assignees)
-        date_to_be_completed = st.date_input("Date to be Completed")
-        task_description = st.text_area("Task Description")
-        add_button = st.button("Add Task")
+        st.title("Organization Task Dashboard")
+
+        # Sponsorship Database
+        st.header("Sponsorship Database")
+        # Get the Notion page URL
+        # Create a markdown element
+        st.write(
+            "Link to sponsorship database [link](https://husam007.notion.site/BMS-Dubai-Main-Dashboard-5d9bd82d7ec34cb8bbff0df0e0cfebdd)")
+
+        # Sidebar to add new tasks
+        authenticator.logout("Logout", "sidebar")
+        st.sidebar.header("Add New Task")
+        new_task = st.sidebar.text_input("Enter task")
+        department = st.sidebar.selectbox("Department Assigned To", departments)
+        assigned_to = st.sidebar.multiselect("Assigned To", assignees)
+        date_to_be_completed = st.sidebar.date_input("Date to be Completed")
+        task_description = st.sidebar.text_area("Task Description")
+        add_button = st.sidebar.button("Add Task")
 
         if add_button and new_task != "":
             task = f"**Task:** {new_task}\n" \
-                f"**Department:** {department}\n" \
-                f"**Assigned To:** {', '.join(assigned_to)}\n" \
-                f"**Date to be Completed:** {date_to_be_completed}\n" \
-                f"**Task Description:** {task_description}"
+                   f"**Department:** {department}\n" \
+                   f"**Assigned To:** {', '.join(assigned_to)}\n" \
+                   f"**Date to be Completed:** {date_to_be_completed}\n" \
+                   f"**Task Description:** {task_description}"
             add_task(task)
-            st.success("Task added successfully!")
+            st.sidebar.success("Task added successfully!")
 
         # Display the list of tasks
         st.header("Tasks")
@@ -67,16 +71,13 @@ if authentication_status:
             st.info("No tasks added yet.")
         else:
             for i, task in enumerate(tasks):
-                st.write(f"{i+1}. {task}")
+                st.write(f"{i + 1}. {task}")
 
                 # Checkbox to mark a task as completed
                 complete_checkbox = st.checkbox("Complete", key=f"complete_checkbox_{i}")
                 if complete_checkbox:
                     complete_task(i)
-                    st.success("Task marked as completed!")
+                    st.sidebar.success("Task marked as completed!")
 
     if __name__ == "__main__":
         main()
-
-else:
-    st.error('Username/password is incorrect')
