38c583b4a3b4720c9ebd9f7a0a72c6a9397dafca
Mohammed Husamuddin <uni.husam@gmail.com>
2023-06-11 20:56:57 +0400

    Push

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,36 @@
 import streamlit as st
 import pickle
 from pathlib import Path
+
 import streamlit_authenticator as stauth
+
+#User Authentication
+names = ["Snehaunshu","Majid","Husam","Aaryan","Drashti","Sai","Keane","Marshal","Sanjay"]
+usernames = ["Snehaunshu","Majid","Husam","Aaryan","Drashti","Sai","Keane","Marshal","Sanjay"]
 
 # Load the hashed passwords
 file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("rb") as file:
     hashed_passwords = pickle.load(file)
 
-# Convert usernames to lowercase
-usernames = ["snehaunshu", "majid", "husam", "aaryan", "drashti", "sai", "keane", "marshal", "sanjay"]
+authenticator = stauth.Authenticate(**{'names': names}, cookie_expiry_days=1)
 
-authenticator = stauth.Authenticate(hashed_passwords, "random_cookie_name", "random_signature_key", cookie_expiry_days=1, usernames=usernames)
+
+
 
 name, authentication_status, username = authenticator.login("Login", "sidebar")
 
-if authentication_status is False:
+if authentication_status == False:
     st.error("The Username/Password Entered is Incorrect, Please Contact HUSAM for Your LogIn Credentials")
-elif authentication_status is None:
+if authentication_status == None:
     st.warning("Please enter your credentials to login")
-elif authentication_status:
-    # Rest of your code...
-    # (Add tasks, departments, assignees, main function, etc.)
-
+if authentication_status:
 
     # Create a list to store tasks
     tasks = []
 
     # Departments
-    departments = ['Technical', 'Marketing', 'Office Bearers', 'Sponsorships']
+    departments = ['Technical', 'Marketing', 'Office Bearers','Sponsorships']
 
     # Assignees
     assignees = ['HUSAM', 'Sanjay', 'Marshal', 'Sai']
@@ -46,15 +48,14 @@ elif authentication_status:
     def main():
         st.title("Organization Task Dashboard")
 
-        # Sponsorship Database
+        #Sponsorship Database
         st.header("Sponsorship Database")
         # Get the Notion page URL
         # Create a markdown element
-        st.write(
-            "Link to sponsorship database [link](https://husam007.notion.site/BMS-Dubai-Main-Dashboard-5d9bd82d7ec34cb8bbff0df0e0cfebdd)")
+        st.write("Link to sponsorship databse [link](https://husam007.notion.site/BMS-Dubai-Main-Dashboard-5d9bd82d7ec34cb8bbff0df0e0cfebdd)")
 
         # Sidebar to add new tasks
-        authenticator.logout("Logout", "sidebar")
+        authenticator.logout("Logout","sidebar")
         st.sidebar.header("Add New Task")
         new_task = st.sidebar.text_input("Enter task")
         department = st.sidebar.selectbox("Department Assigned To", departments)
@@ -65,10 +66,10 @@ elif authentication_status:
 
         if add_button and new_task != "":
             task = f"**Task:** {new_task}\n" \
-                   f"**Department:** {department}\n" \
-                   f"**Assigned To:** {', '.join(assigned_to)}\n" \
-                   f"**Date to be Completed:** {date_to_be_completed}\n" \
-                   f"**Task Description:** {task_description}"
+                f"**Department:** {department}\n" \
+                f"**Assigned To:** {', '.join(assigned_to)}\n" \
+                f"**Date to be Completed:** {date_to_be_completed}\n" \
+                f"**Task Description:** {task_description}"
             add_task(task)
             st.sidebar.success("Task added successfully!")
 
@@ -78,7 +79,7 @@ elif authentication_status:
             st.info("No tasks added yet.")
         else:
             for i, task in enumerate(tasks):
-                st.write(f"{i + 1}. {task}")
+                st.write(f"{i+1}. {task}")
 
                 # Checkbox to mark a task as completed
                 complete_checkbox = st.checkbox("Complete", key=f"complete_checkbox_{i}")
