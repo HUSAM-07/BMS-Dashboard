d1f41984aedef738ec2c1ed8b9375461a82f0914
Mohammed Husamuddin <uni.husam@gmail.com>
2023-06-11 18:59:42 +0400

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("rb") as file:
     hashed_passwords = pickle.load(file)
 
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "BMS Dashboard", "abcdef", cookie_expiry_days=30)
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "bms_dashboard", "abcdef", cookie_expiry_days=30)
+
 
 name, authentication_status, username = authenticator.login("Login", "sidebar")
 
