111778861cbd5814fbfd8513b63b9947882ad401
Mohammed Husamuddin <uni.husam@gmail.com>
2023-06-11 18:45:00 +0400

    User Authentication

---
+++ generate_keys.py
@@ -0,0 +1,14 @@
+import pickle
+from pathlib import Path
+import streamlit_authenticator as stauth
+
+# List of the people who have access to the App
+
+names = ["Snehaunshu","Majid","Husam","Aaryan","Drashti","Sai","Keane","Marshal","Sanjay"]
+usernames = ["Snehaunshu","Majid","Husam","Aaryan","Drashti","Sai","Keane","Marshal","Sanjay"]
+passwords = ["Snehaunshu","Majid","Husam","Aaryan","Drashti","Sai","Keane","Marshal","Sanjay"]
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("wb") as file:
+    pickle.dump(hashed_passwords, file)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,65 +1,88 @@
 import streamlit as st
+import pickle
+from pathlib import Path
+import streamlit_authenticator as stauth
 
-# Create a list to store tasks
-tasks = []
+#User Authentication
+names = ["Snehaunshu","Majid","Husam","Aaryan","Drashti","Sai","Keane","Marshal","Sanjay"]
+usernames = ["Snehaunshu","Majid","Husam","Aaryan","Drashti","Sai","Keane","Marshal","Sanjay"]
 
-# Departments
-departments = ['Technical', 'Marketing', 'Office Bearers','Sponsorships']
+# Load the hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
 
-# Assignees
-assignees = ['HUSAM', 'Sanjay', 'Marshal', 'Sai']
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "BMS Dashboard", "abcd", cookie_expiry_days=0)
 
-# Function to add a task
-def add_task(task):
-    tasks.append(task)
+name, authentication_status, username = authenticator.login("login","main")
 
-# Function to mark a task as completed
-def complete_task(task_index):
-    if task_index < len(tasks):
-        tasks[task_index] = f"✅ {tasks[task_index]}"
+if authentication_status == False:
+    st.error("The Username/Password Entered is Incorrect, Please Contact HUSAM for Your LogIn Credentials")
+if authentication_status == None:
+    st.warning("Please enter your credentials to login")
+if authentication_status:
 
-# Streamlit app layout
-def main():
-    st.title("Organization Task Dashboard")
+    # Create a list to store tasks
+    tasks = []
 
-    #Sponsorship Database
-    st.header("Sponsorship Database")
-    # Get the Notion page URL
-    # Create a markdown element
-    st.write("Link to sponsorship databse [link](https://husam007.notion.site/BMS-Dubai-Main-Dashboard-5d9bd82d7ec34cb8bbff0df0e0cfebdd)")
+    # Departments
+    departments = ['Technical', 'Marketing', 'Office Bearers','Sponsorships']
 
-    # Sidebar to add new tasks
-    st.sidebar.header("Add New Task")
-    new_task = st.sidebar.text_input("Enter task")
-    department = st.sidebar.selectbox("Department Assigned To", departments)
-    assigned_to = st.sidebar.multiselect("Assigned To", assignees)
-    date_to_be_completed = st.sidebar.date_input("Date to be Completed")
-    task_description = st.sidebar.text_area("Task Description")
-    add_button = st.sidebar.button("Add Task")
+    # Assignees
+    assignees = ['HUSAM', 'Sanjay', 'Marshal', 'Sai']
 
-    if add_button and new_task != "":
-        task = f"**Task:** {new_task}\n" \
-               f"**Department:** {department}\n" \
-               f"**Assigned To:** {', '.join(assigned_to)}\n" \
-               f"**Date to be Completed:** {date_to_be_completed}\n" \
-               f"**Task Description:** {task_description}"
-        add_task(task)
-        st.sidebar.success("Task added successfully!")
+    # Function to add a task
+    def add_task(task):
+        tasks.append(task)
 
-    # Display the list of tasks
-    st.header("Tasks")
-    if len(tasks) == 0:
-        st.info("No tasks added yet.")
-    else:
-        for i, task in enumerate(tasks):
-            st.write(f"{i+1}. {task}")
+    # Function to mark a task as completed
+    def complete_task(task_index):
+        if task_index < len(tasks):
+            tasks[task_index] = f"✅ {tasks[task_index]}"
 
-            # Checkbox to mark a task as completed
-            complete_checkbox = st.checkbox("Complete", key=f"complete_checkbox_{i}")
-            if complete_checkbox:
-                complete_task(i)
-                st.sidebar.success("Task marked as completed!")
+    # Streamlit app layout
+    def main():
+        st.title("Organization Task Dashboard")
 
-if __name__ == "__main__":
-    main()
+        #Sponsorship Database
+        st.header("Sponsorship Database")
+        # Get the Notion page URL
+        # Create a markdown element
+        st.write("Link to sponsorship databse [link](https://husam007.notion.site/BMS-Dubai-Main-Dashboard-5d9bd82d7ec34cb8bbff0df0e0cfebdd)")
+
+        # Sidebar to add new tasks
+        authenticator.logout("Logout","sidebar")
+        st.sidebar.header("Add New Task")
+        new_task = st.sidebar.text_input("Enter task")
+        department = st.sidebar.selectbox("Department Assigned To", departments)
+        assigned_to = st.sidebar.multiselect("Assigned To", assignees)
+        date_to_be_completed = st.sidebar.date_input("Date to be Completed")
+        task_description = st.sidebar.text_area("Task Description")
+        add_button = st.sidebar.button("Add Task")
+
+        if add_button and new_task != "":
+            task = f"**Task:** {new_task}\n" \
+                f"**Department:** {department}\n" \
+                f"**Assigned To:** {', '.join(assigned_to)}\n" \
+                f"**Date to be Completed:** {date_to_be_completed}\n" \
+                f"**Task Description:** {task_description}"
+            add_task(task)
+            st.sidebar.success("Task added successfully!")
+
+        # Display the list of tasks
+        st.header("Tasks")
+        if len(tasks) == 0:
+            st.info("No tasks added yet.")
+        else:
+            for i, task in enumerate(tasks):
+                st.write(f"{i+1}. {task}")
+
+                # Checkbox to mark a task as completed
+                complete_checkbox = st.checkbox("Complete", key=f"complete_checkbox_{i}")
+                if complete_checkbox:
+                    complete_task(i)
+                    st.sidebar.success("Task marked as completed!")
+
+    if __name__ == "__main__":
+        main()
 
